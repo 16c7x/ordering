4b8f6e62064d1aba61cfb5522e995fcb5b8a8252
Andrew Jones <pic16c7x@gmail.com>
2020-11-26 12:53:32 +0000

    Create users.pp

---
+++ manifests/users.pp
@@ -0,0 +1,15 @@
+class ordering::users {
+$admins  = { 
+    'brad'   => { password => 'myPass123' },
+    'monica' => { password => 'myPass123' },
+    'luke'   => { password => 'myPass123' },
+    'zack'   => { password => 'sqlPass123' },
+  }
+
+  $admins.each |String $user, Hash $params| {
+    sqlserver::user { $user:
+      ensure   => present,
+      database => 'mydb',
+    }
+  }
+}
