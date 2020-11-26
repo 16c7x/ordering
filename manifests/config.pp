8abf386953da2c75c0c7726dfdb2d4c4e7915b4c
Andrew Jones <pic16c7x@gmail.com>
2020-11-26 18:25:45 +0000

    Update config.pp

--- manifests/config.pp
+++ manifests/config.pp
@@ -11,7 +11,7 @@ class ordering::config {
     require          => Sqlserver_instance['MSSQLSERVER'],
   }
 
-  sqlserver::database { 'mydb13':
+  sqlserver::database { 'mydb14':
     ensure  => present,
     require => Sqlserver::Config['MSSQLSERVER'],
   }
