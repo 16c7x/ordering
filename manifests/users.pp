1efcb468886ab96604e9eb02064f6e872ff4c527
Andrew Jones <pic16c7x@gmail.com>
2020-11-26 17:56:00 +0000

    Update users.pp

--- manifests/users.pp
+++ manifests/users.pp
@@ -9,7 +9,7 @@ $admins  = {
   $admins.each |String $user, Hash $params| {
     sqlserver::user { $user:
       ensure   => present,
-      database => 'mydb3',
+      database => 'mydb2',
     }
   }
 }
